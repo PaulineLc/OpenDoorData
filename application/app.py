90aa82e2b48c91c5cebb201b18fbe1f84e7f5c3b
Jack Halpin <jack.halpin.1@ucdconnect.ie>
2016-06-22 16:07:44 +0100

    app connects to mysql database

--- application/app.py
+++ application/app.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from flask import render_template
+from flask_mysqldb import MySQL
+
+
 app = Flask(__name__)
+mysql = MySQL(app)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'finaltest'
+#mysql.init_app(app)
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def render():
+	cur = mysql.connection.cursor()
+	cur.execute('''SELECT * FROM candidates''')
+	rv = cur.fetchall()
+	cur.close()
+	return str(rv)
 
 if __name__ == '__main__':
     app.run(debug = True)
