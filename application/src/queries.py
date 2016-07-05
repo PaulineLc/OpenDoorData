a5a499b1733f9d0e7b0f8b3b16a8f9580a6767ed
Jack Halpin <jack.halpin.1@ucdconnect.ie>
2016-07-05 20:38:46 +0100

    queries now works

--- application/src/queries.py
+++ application/src/queries.py
@@ -8,7 +8,12 @@ def hourly_average(room, day):
 	chosen_day = getDay(day)
 	conn = pymysql.connect(host='localhost', user='root', password='summer')
 	c = conn.cursor()
-	c.execute('SELECT round(avg(assoc_devices)) FROM wifi_db.wifi_log WHERE room_id = 2 AND FROM_UNIXTIME(event_time,"%w") = 1 AND FROM_UNIXTIME(event_time,"%H") >= 08 GROUP BY FROM_UNIXTIME(event_time,"%H")')
+	c.execute("""
+    SELECT round(avg(assoc_devices)) 
+    FROM wifi_db.wifi_log 
+    WHERE room_id = 2 AND 
+          FROM_UNIXTIME(event_time,"%%w") = %s""", (chosen_day,))
+
 	data = c.fetchall()
 	for i in data:
 		hourly_data.append(str(i[0]))
@@ -22,5 +27,5 @@ def hourly_average(room, day):
 def getDay(day):
         '''Returns the day of the week as it is represented in SQLite as a string that can be used for comparison'''
         days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-        return str(days.index(day))
+        return days.index(day)
 
