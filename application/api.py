407a6bc47e25ff2cef396a4363160a2f24cac12b
DonBlaine <donovanjblaine@gmail.com>
2016-07-05 01:01:25 +0100

    started adding code to api,main,models for testing purposes

--- application/api.py
+++ application/api.py
@@ -1,1 +1,15 @@
-api.py - Where you register models to be exposed via a REST-ful API
+#api.py - Where you register models to be exposed via a REST-ful API
+
+from flask_peewee.rest import RestAPI
+from models import room, survey
+from app import app # our project's Flask app
+
+# instantiate our api wrapper
+api = RestAPI(app)
+
+# register our models so they are exposed via /api/<model>/
+api.register(room)
+api.register(survey)
+
+# configure the urls
+#api.setup()
--- application/main.py
+++ application/main.py
@@ -1,1 +1,16 @@
-entry point to site
+#entry point to site
+
+from app import app
+
+#from auth import *
+#from admin import admin
+from api import api
+from models import *
+#from views import *
+
+#admin.setup()
+api.setup()
+
+
+if __name__ == '__main__':
+    app.run()
