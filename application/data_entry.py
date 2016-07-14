aa94bb832b7a33406145fbcc897fa33f31ddc65a
DonBlaine <donovanjblaine@gmail.com>
2016-07-14 19:43:37 +0100

    adding foreign constraint fields to models.py

--- application/data_entry.py
+++ application/data_entry.py
@@ -1,4 +1,3 @@
-
 import csv
 import time as tm
 import datetime
@@ -7,7 +6,6 @@ import models
 
 #useful for viewing the specific sql queries to debug
 import logging
-from models import room
 logger = logging.getLogger('peewee')
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler())
@@ -28,14 +26,14 @@ def main():
         
         return (building,room)
     
-    models.db.connect()
-    models.db.create_tables([models.User,
-                             models.room,
-                             models.wifi_log,
-                             models.module,
-                             models.timetable,
-                             models.survey,
-                             models.wifiStudents], safe=True)
+    models.db.create_tables([models.room,
+                              models.User,
+                              models.module,
+                              models.wifiStudents,
+                              models.wifi_log,
+                              models.timetable,
+                              models.survey,
+                              ], safe=True)
     
     models.room.create(room_num = 2,
                 building = "school of computer science",
@@ -52,6 +50,8 @@ def main():
     models.User.create(username = "admin",
                  password = "password",
                  email = "donovanjblaine@gmail.com",
+                 first_name = 'Don',
+                 last_name = "Blaine",
                  admin = True
                  )
     user = models.User.get(models.User.username == "admin")
@@ -74,7 +74,7 @@ def main():
                 continue
             else:
                 models.module.create(module_code = modulecode,
-                                     instructor = None
+                                     instructor = user.username
                                      )
                            
             modlist.append(modulecode)
@@ -83,11 +83,11 @@ def main():
          
         roomid = mylist[i][1]
         build = mylist[i][5]
-        roomnum = room.get(room.room_num == roomid, room.building == build)
         time1 = int(mylist[i][2])
         reg_stu = mylist[i][4] if mylist[i][4]!= "" else 0
         modulecode = mylist[i][3] if len(mylist[i][3])>1 else None
-        models.timetable.create(room_id = roomnum.id_field,
+        models.timetable.create(room_id = roomid,
+                                building = build,
                          mod_code = modulecode,
                          event_time = time1,
                          reg_stu = int(float(reg_stu)),
@@ -105,9 +105,9 @@ def main():
     for i in range(len(mylist)):
         roomid = int(parseName(mylist[i][0])[1])
         build = "school of " + parseName(mylist[i][0])[0]
-        roomnum = room.get(room.room_num == roomid, room.building == build)
         etime = float(epochtime(mylist[i][1]))
-        models.wifi_log.create(room_id = roomnum.id_field,
+        models.wifi_log.create(room_id = roomid,
+                                building = build,
                             event_time = etime, 
                             assoc_devices = mylist[i][2], 
                             auth_devices = mylist[i][3],
@@ -125,11 +125,12 @@ def main():
     for i in range(1, len(mylist)):
         roomid = mylist[i][1]
         build = mylist[i][4]
-        roomnum = room.get(room.room_num == roomid, room.building == build)
         etime = int(mylist[i][2])
-        models.survey.create(room_id = roomnum.id_field,
+        models.survey.create(room_id = roomid,
+                              building = build,
                         event_time = etime,
                         occupancy = mylist[i][3],
+                        reporter = user.username,
                         time = datetime.datetime.fromtimestamp(etime)
                         )
          
@@ -140,3 +141,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
--- application/models.py
+++ application/models.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     conn.close()
 
 configdb = app.config['DATABASE']
-db = peewee.MySQLDatabase(configdb['name'],
+db = peewee.MySQLDatabase("wifi_db",
                           host = configdb['host'],
                           user = configdb['user'],
                           password =configdb['password']
@@ -41,88 +41,110 @@ class room(BaseModel):
         indexes = (
                    (('room_num', 'building'), True),
                    )
-            
-    def __unicode__(self):
-        return self.id_field
+
     def __str__(self):
         return str(self.id_field)
     
 class wifi_log(BaseModel):
     id_field = peewee.PrimaryKeyField()
-    room_id = peewee.ForeignKeyField(room,to_field='id_field', db_column='room_id', on_delete='CASCADE')
+    room_id = peewee.IntegerField()
     event_time = peewee.IntegerField()
     assoc_devices = peewee.IntegerField()
     auth_devices = peewee.IntegerField()
     time = peewee.DateTimeField()
+    building = peewee.CharField()
   
     class Meta:
-        indexes = ((('room_id','event_time'), True),)
+        indexes = ((('room_id','event_time','building'), True),)
+        constraints = [peewee.SQL('FOREIGN KEY(room_id, building)'
+                           'REFERENCES room(room_num, building)ON DELETE CASCADE ON UPDATE CASCADE')]
+    def __str__(self):
+        return str(self.id_field)
     
 class User(BaseModel, BaseUser):
     #need to have auto id for sessions to work
     username = peewee.CharField(unique=True)
     password = peewee.CharField()
+    first_name = peewee.CharField()
+    last_name=peewee.CharField()
     email = peewee.CharField()
     join_date = peewee.DateTimeField(default=datetime.datetime.now)
     active = peewee.BooleanField(default=True)
     admin = peewee.BooleanField(default=False)
-    
-    def __unicode__(self):
-        return self.username
+
        
     def __str__(self):
         return str(self.username)
                
 class module(BaseModel):
     module_code = peewee.CharField(primary_key=True)
-    instructor = peewee.ForeignKeyField(User, db_column = 'instructor', null=True, on_delete='SET NULL')
+    instructor = peewee.ForeignKeyField(User,
+                                        to_field='username', 
+                                        db_column = 'instructor', 
+                                        null=True, 
+                                        on_delete='SET NULL',
+                                        on_update='CASCADE')
     
-    def __unicode__(self):
-        return self.module_code
       
     def __str__(self):
         return str(self.module_code)
 
-
 class timetable(BaseModel):
     id_field = peewee.PrimaryKeyField()
-    room_id = peewee.ForeignKeyField(room,to_field='id_field', db_column='room_id', on_delete='CASCADE')
-    mod_code = peewee.ForeignKeyField(module, to_field='module_code', db_column='mod_code',null=True, on_delete='SET NULL')
+    room_id = peewee.IntegerField()
+    mod_code = peewee.ForeignKeyField(module, 
+                                      to_field='module_code', 
+                                      db_column='mod_code',
+                                      null=True, 
+                                      on_delete='SET NULL')
     event_time = peewee.IntegerField()
     reg_stu = peewee.IntegerField()
     time = peewee.DateTimeField()
+    building = peewee.CharField()
     
-    def __unicode__(self):
-        return self.id_field   
+    class Meta:
+        indexes = ((('room_id','event_time','building'), True),)
+        constraints = [peewee.SQL('FOREIGN KEY(room_id, building)'
+                           'REFERENCES room(room_num, building)')]   
     def __str__(self):
-        return str(self.id_field) 
+        return str(self.id_field)
 
 class survey(BaseModel):
     id_field = peewee.PrimaryKeyField()
-    room_id = peewee.ForeignKeyField(room,to_field='id_field', db_column='room_id', on_delete='CASCADE')
+    room_id = peewee.IntegerField()
     event_time = peewee.IntegerField()
     occupancy = peewee.FloatField(constraints=[peewee.Check('occupancy <= 1 AND occupancy >=0')])
     time = peewee.DateTimeField()
-    instructor = peewee.ForeignKeyField(User, db_column = 'instructor', null=True, on_delete='SET NULL')
-    
-
+    reporter = peewee.ForeignKeyField(User,
+                                      to_field='username', 
+                                        db_column = 'reporter', 
+                                        null=True, 
+                                        on_delete='SET NULL',
+                                        on_update='CASCADE')
+    building = peewee.CharField()
     
     class Meta:
-        indexes = ((('room_id','event_time'), True),)
+        indexes = ((('room_id','event_time','building'), True),)
+        constraints = [peewee.SQL('FOREIGN KEY(room_id, building)'
+                                  'REFERENCES room(room_num, building)'
+                                  'ON DELETE CASCADE ON UPDATE CASCADE')]
+
         
 class wifiStudents(BaseModel):
     id_field = peewee.PrimaryKeyField()
-    room_id = peewee.ForeignKeyField(room,to_field='id_field', db_column='room_id', on_delete='CASCADE')
+    room_id = peewee.IntegerField()
     event_time = peewee.IntegerField()
     occupancy = peewee.DecimalField(constraints=[peewee.Check('occupancy <= 1 AND occupancy >=0')])
+    building = peewee.CharField()
     time = peewee.DateTimeField()
 
     class Meta:
-        indexes = ((('room_id','event_time'), True),)
+        indexes = ((('room_id','event_time','building'), True),)
+        constraints = [peewee.SQL('FOREIGN KEY(room_id, building)'
+                           'REFERENCES room(room_num, building) ON DELETE CASCADE ON UPDATE CASCADE')]
         
 
 db.close()
-
 
 
 
