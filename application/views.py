6850bbff05ef73b2b97fe8a27e0450aefa18dcd5
Jack Halpin <jhalpin92@gmail.com>
2016-07-25 12:31:25 +0100

    added view

--- application/views.py
+++ application/views.py
@@ -1,7 +1,8 @@
 #views.py - Views that handle requests.
 
-from flask import render_template,request  
-
+from flask import render_template  
+from src import json_creator
+from src import queries
 from myapp import app
 from auth import auth
 from models import room
@@ -24,5 +25,13 @@ def rendersurvey():
     rooms= room.select()
     #cur.close().
     return render_template("survey.html", 
-                           rooms=rooms,user=user,password = user.password)
+                           rooms=rooms)
 
+
+@app.route('/dailyavg/<rid>')
+def returnDailyStats(rid):
+	daily_averages = queries.daily_average(rid)
+	frequency_of_use = queries.frequency_of_use(rid)
+	jdata = json_creator.createRoomJson(daily_averages, frequency_of_use)
+	print(jdata)
+	return jdata
