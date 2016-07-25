38b86d6b29fab504ebfd8650f19c8115a63d6459
Jack Halpin <jhalpin92@gmail.com>
2016-07-25 12:34:59 +0100

    updated to handle multiple lists

--- application/src/json_creator.py
+++ application/src/json_creator.py
@@ -6,3 +6,23 @@ def return_json(data):
 	j = json.dumps(data)
 
 	return j
+
+def createRoomJson(daily_average, frequency_of_use):
+        
+        
+    
+    output_list = []
+    
+
+    d = collections.OrderedDict()
+    d['Daily'] = daily_average
+    d['Frequency'] = frequency_of_use
+
+        
+        
+        
+
+    output_list.append(d) 
+    
+    j = json.dumps(output_list)
+    return j
