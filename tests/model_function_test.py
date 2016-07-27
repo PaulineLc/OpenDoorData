fdf32b80bf964e899c3b79650bc893faf8938ced
Pauline Lallinec <12251066@ucdconnect.ie>
2016-07-27 11:40:45 +0100

    first test passed

--- tests/model_function_test.py
+++ tests/model_function_test.py
@@ -6,9 +6,10 @@ from application.model_functions import *
 class Testing(unittest.TestCase):
 
     def test_isempty(self):
-        df = pd.DataFrame()
-        self.assertTrue(isempty_df(df))
-
+        df1 = pd.DataFrame()
+        self.assertTrue(isempty_df(df1))
+        df2 = pd.DataFrame([[0,'abcd',0,1,123]],columns=['a','b','c','d','e'])
+        self.assertFalse(isempty_df(df2))
 
     def test_convert_to_epoch(self):
         pass
