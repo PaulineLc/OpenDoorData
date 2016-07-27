2d84ab5b71adc80c8e5c5324677a36c2233d89ff
Pauline Lallinec <12251066@ucdconnect.ie>
2016-07-27 11:23:37 +0100

    created skeleton for model_functions_test.py

--- tests/model_function_test.py
+++ tests/model_function_test.py
@@ -3,4 +3,22 @@ import unittest
 from application.model_functions import *
 
 class Testing(unittest.TestCase):
-    def
+
+    def test_isempty(self):
+        pass
+
+    def test_convert_to_epoch(self):
+        pass
+
+    def room_number(self):
+        pass
+
+    def estimate_occ(self):
+        pass
+
+    def dataframe_epochtime_to_datetime(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
