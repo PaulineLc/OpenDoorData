46435f8ed823884ee52a343618aa9f10fa3c0cc5
Jack Halpin <jack.halpin.1@ucdconnect.ie>
2016-08-03 16:31:52 +0100

    added functions

--- application/static/data_calculations.js
+++ application/static/data_calculations.js
@@ -7,3 +7,27 @@ function convertToPercentage(data, capacity){
 	}
 	return data
 }
+
+function getFrequencyPercentage(freq){
+	console.log(freq[1]);
+	var sum = parseInt(freq[0]) + parseInt(freq[1]);
+	console.log(sum);
+	var frequency_percentage = Math.round(freq[0] / sum * 100);
+	console.log(frequency_percentage);
+	document.getElementById("general_fou_percent").innerHTML = frequency_percentage.toString() + "%";
+}
+
+function setOccupancyPercentage(occu){
+	var sum = 0;
+	console.log(occu[0].occupancy_category_5);
+	for (var i  = 0; i < occu.length; i++){
+		sum += occu[i].occupancy_category_5;
+	}
+	var occupancy_percentage = Math.round((sum / occu.length) * 100);
+	var occupancy_data_points = []
+	occupancy_data_points.push(occupancy_percentage);
+	document.getElementById("general_occupancy_percent").innerHTML = occupancy_percentage.toString() + "%";
+	occupancy_data_points.push(100 - occupancy_percentage);
+	console.log(occupancy_data_points);
+	return occupancy_data_points;
+}
