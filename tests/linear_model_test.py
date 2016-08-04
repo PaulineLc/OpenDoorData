eba1ada40151d812058376f8923b8b24b528d3fe
Elayne Ruane <elayne.ruane@ucdconnect.ie>
2016-08-04 12:46:57 +0100

    Added test function to test if output of get_linear_coef is of type float

--- tests/linear_model_test.py
+++ tests/linear_model_test.py
@@ -18,9 +18,21 @@ class Testing(unittest.TestCase):
         file_2 = 'survey_data_test'
         
         # test output
-        expected_result = 1
+        expected_result = 1.0
         actual_result = get_linear_coef(folder, file_1, file_2)
         self.assertEqual(expected_result, actual_result)
+        
+    def test_get_linear_coef_output(self):
+        # test inputs
+        folder = 'test_data'
+        file_1 = 'full_test'
+        file_2 = 'survey_data_test'
+        # test output
+        output = get_linear_coef(folder, file_1, file_2)
+        # compare
+        output_type = type(output)
+        expected_type = 'float'
+        self.assertEqual(output_type, expected_type)
     
     
 if __name__ == '__main__':
