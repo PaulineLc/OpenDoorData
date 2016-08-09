68bb0a48eb1c43b995afca64d85babfd32350c7f
Jack Halpin <jack.halpin.1@ucdconnect.ie>
2016-08-09 17:57:16 +0100

    added comments

--- application/static/data_calculations.js
+++ application/static/data_calculations.js
@@ -9,56 +9,40 @@ function convertToPercentage(data, capacity){
 }
 
 function getFrequencyPercentage(freq){
-	console.log(freq[1]);
+	//This function calculates and displays the frequency of use of a particular room according to the JSON data
 	var sum = parseInt(freq[0]) + parseInt(freq[1]);
-	console.log(sum);
 	var frequency_percentage = Math.round(freq[0] / sum * 100);
-	console.log(frequency_percentage);
 	document.getElementById("general_fou_percent").innerHTML = frequency_percentage.toString() + "%";
 }
 
 function setOccupancyPercentage(occu){
+	//This function calculates and displays the occupancy rating of a particular room according to the JSON data
 	var sum = 0;
-	console.log(occu[0].occupancy_category_5);
 	for (var i  = 0; i < occu.length; i++){
 		sum += occu[i].occupancy_category_5;
 	}
+
 	var occupancy_percentage = Math.round((sum / occu.length) * 100);
 	var occupancy_data_points = []
 	occupancy_data_points.push(occupancy_percentage);
+
 	document.getElementById("general_occupancy_percent").innerHTML = occupancy_percentage.toString() + "%";
 	occupancy_data_points.push(100 - occupancy_percentage);
-	console.log(occupancy_data_points);
 	return occupancy_data_points;
 }
 
 function calculateYaxisLabels(minfo){
+	//Based on the times supplied in the JSON data this function will create the Y axis labels that are used on the historial 
+	//module data graph
+
 	//initiate labels to an empty array
 	var labels = [];
 
 	//loop through each class that has been recorded and create a yaxis label string consisting of the date format = 'day' of 'month' 'yea' @ 'TIME'
 	for (var i = 0; i < minfo.length; i++){
-		var dateStr = minfo[i].event_day + " " + getMonth(minfo[i].event_month) + " " + minfo[i].event_year + " @ " + minfo[i].event_hour;
+		var dateStr = minfo[i].event_day + "/" + minfo[i].event_month + "/" + minfo[i].event_year + " @ " + minfo[i].event_hour;
 		labels.push(dateStr);
 	}
 
 	return labels;
-}
-
-function getMonth(m_int){
-	var month = new Array();
-	month[1] = "January";
-	month[2] = "February";
-	month[3] = "March";
-	month[4] = "April";
-	month[5] = "May";
-	month[6] = "June";
-	month[7] = "July";
-	month[8] = "August";
-	month[9] = "September";
-	month[10] = "October";
-	month[11] = "November";
-	month[12] = "December";
-
-	return month[m_int];
 }
