825cc8e979194e028f25c4d00c1715a79df87941
Pauline Lallinec <12251066@ucdconnect.ie>
2016-08-15 13:41:21 +0100

    final version of the Android app prototype

--- AndroidStudioProjects/Everythingisawesome/app/src/main/java/com/example/root/everythingisawesome/MainActivity.java
+++ AndroidStudioProjects/Everythingisawesome/app/src/main/java/com/example/root/everythingisawesome/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Thread t = new Thread() {
 
@@ -24,27 +26,32 @@ public class MainActivity extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                String my_text = getMacId();
-                                TextView my_textview = (TextView) findViewById(R.id.textView1);
+                                if (isConnectedToWifi()) {
 
-                                String currentRoom = "";
+                                    String currentBSSID = getMacId();
+                                    TextView textView = (TextView) findViewById(R.id.textView1);
 
-                                if (my_text.equals("6c:99:89:99:cc:3f")) {
-                                    currentRoom = "B0.02";
-                                } else if (my_text.equals("6c:99:89:a1:e5:af")) {
-                                    currentRoom = "B0.03";
-                                } else if (my_text.equals("6c:99:89:99:d7:0f")) {
-                                    currentRoom = "B0.04";
+                                    String currentRoom = "";
+
+                                    if (currentBSSID.equals("6c:99:89:99:cc:3f")) {
+                                        currentRoom = "B0.02";
+                                    } else if (currentBSSID.equals("6c:99:89:a1:e5:af")) {
+                                        currentRoom = "B0.03";
+                                    } else if (currentBSSID.equals("6c:99:89:99:d7:0f")) {
+                                        currentRoom = "B0.04";
+                                    } else {
+                                        currentRoom = "unknown room";
+                                    }
+
+                                    textView.setText("BSSID:" + currentBSSID +
+                                            "\n" +
+                                            "Current room:" + currentRoom + "\n" +
+                                            "Signal strenght: " + getSignalStrenght() +
+                                            "\nTime: " + System.currentTimeMillis());
                                 } else {
-                                    currentRoom = "unknown room";
+                                    TextView my_textview = (TextView) findViewById(R.id.textView1);
+                                    my_textview.setText("Wifi disconnected");
                                 }
-
-                                my_textview.setText("BSSID:" + my_text +
-                                        "\n" +
-                                        "Current room:\n" +
-                                        currentRoom + "\n" +
-                                        "Signal strenght: " + getSignalStrenght() +
-                                        "\nTime: " + System.currentTimeMillis());
                             }
                         });
                     }
@@ -57,8 +64,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public String getMacId() {
-
         WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        //wifiManager.reassociate();
         WifiInfo wifiInfo = wifiManager.getConnectionInfo();
         return wifiInfo.getBSSID();
     }
@@ -68,5 +75,27 @@ public class MainActivity extends AppCompatActivity {
         WifiInfo wifiInfo = wifiManager.getConnectionInfo();
         return wifiInfo.getRssi();
     }
+
+    public void refreshWifi(View view) {
+        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        wifiManager.reassociate();
+    }
+
+    public boolean isConnectedToWifi() {
+        WifiManager wifiMgr = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        if (wifiMgr.isWifiEnabled()) { // WiFi adapter is ON
+            WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
+            if( wifiInfo.getNetworkId() == -1 ){
+                return false; // Not connected to an access-Point
+            }
+            return true;      // Connected to an Access Point
+        } else {
+            return false; // WiFi adapter is OFF
+        }
+    }
+
+
+
+
 
 }
