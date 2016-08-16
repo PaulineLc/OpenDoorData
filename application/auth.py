9bbb67207298eaea2ec2ea439c12e0ba764f171e
Elayne Ruane <elayne.ruane@ucdconnect.ie>
2016-08-16 14:58:31 +0100

    added comments to auth file

--- application/auth.py
+++ application/auth.py
@@ -1,9 +1,8 @@
-#auth.py - The authentication system used to protect access to the admin.
+# file that contains the authentication system used to provide security to the admin interface
 
 from flask_peewee.auth import Auth  # Login/logout views, etc.
-
 from app import app
 from models import User, db
 
-
+# instantiate an Auth object for use with flask app and database wrapper
 auth = Auth(app, db, user_model = User)
