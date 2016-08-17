09f9178b0e38775868611ffcb56f8938d324026e
Elayne Ruane <elayne.ruane@ucdconnect.ie>
2016-08-17 01:40:50 +0100

    added comments to json_creator.py, made imports more specific, reviewed modularity of code

--- application/src/json_creator.py
+++ application/src/json_creator.py
@@ -1,47 +1,49 @@
-import json
-import collections
+# file containing functions that return json data
 
-#TODO: These functions are highly repetitive, can they be made more modular
+from json import dumps # function that serializes obj to a JSON formatted str using a conversion table
+from collections import OrderedDict # dict subclass that remembers the order entries are added
 
 def createGeneralDataJson(hourly_average, frequency_of_use, occupancy_rating):
 	'''Function that takes in the hourly average data created from occupancy_prediction.py and the frequency of use data
 	for a room and returns the two parameters merged into one JSON file'''
 
-	output_list = []
+	output_list = [] # create empty list
 
-	d = collections.OrderedDict()
+	d = OrderedDict() #initialise d as empty ordered list
 	d['Daily'] = hourly_average
 	d['Frequency'] = frequency_of_use
 	d['Occupancy_Rating'] = occupancy_rating
 
 	output_list.append(d)
-	j = json.dumps(output_list)
+	j = dumps(output_list)
 	return j
 
 def createBuildingInfoJson(building_info, room_info):
 	'''Function that takes in a queried building's information and a list of all rooms with the rooms capacity
 	and returns the two parameters merged into one JSON file'''
+ 
 	output_list = []
 
-	d = collections.OrderedDict()
+	d = OrderedDict()
 	d['building_info'] = building_info
 	d['room_list'] = room_info
+ 
 	output_list.append(d)
-
-	j = json.dumps(d)
+	j = dumps(d)
 	print(j)
 	return j
 
 def returnModuleJSON(m_data, reg_stu):
 	'''Function that takes in a queried module's information and the registered students for that module
 	and returns the two parameters merged into one JSON file'''
+ 
 	output_list = []
 
-	d = collections.OrderedDict()
+	d = OrderedDict()
 	d['module_info'] = m_data
 	d['registered_students'] = reg_stu
 	
 	output_list.append(d)
-	j = json.dumps(d)
+	j = dumps(d)
 	return j
 
